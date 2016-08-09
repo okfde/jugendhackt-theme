083b5eac528e138471d0c2e231499a32b6662c9b
Hubert Hesse <hubert.hesse@hubertscorner.de>
2016-08-09 17:32:16 +0200

    Remove search button in sidebar, fixes #8

--- functions.php
+++ functions.php
@@ -159,7 +159,6 @@ function bones_wpsearch($form) {
 	$form = '<form role="search" method="get" id="searchform" action="' . home_url( '/' ) . '" >
 	<label class="screen-reader-text" for="s">' . __('Search for:', 'bonestheme') . '</label>
 	<input type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="'.esc_attr__('Search the Site...','bonestheme').'" />
-	<input type="submit" id="searchsubmit" value="'. esc_attr__('Search') .'" />
 	</form>';
 	return $form;
 } // don't remove this bracket!
