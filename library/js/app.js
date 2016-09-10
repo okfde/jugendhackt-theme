633f21a78317ea332fd3de5d2bca9df17641c1da
rhotep <andreas.pittrich@posteo.de>
2016-09-11 01:23:06 +0200

    idg

--- library/js/app.js
+++ library/js/app.js
@@ -8,27 +8,26 @@ angular.module('jugendHackt', [])
 	function($http){
 		return {
 			restrict: 	"AE",
-			scope:		{
-							teaserData : "@",
-							hackDashId : "@",
-						},
+			scope:		true,
 
-			link: function($scope){
+			link: function(scope, element, attrs){
 
-				var id_in_embed_snippet  = $scope.hackDashId.match(/hackdash\.org\/embed\/projects\/([a-zA-Z0-9]+)[^a-zA-Z0-9]/)
+				var id_in_embed_snippet  = attrs.hackDashId.match(/hackdash\.org\/embed\/projects\/([a-zA-Z0-9]+)[^a-zA-Z0-9]/)
 
 				if(id_in_embed_snippet != null){
-					$scope.hackDashId = id_in_embed_snippet[1]
+					scope.hackDashId = id_in_embed_snippet[1]
+				} else {
+					scope.hashDashId = attrs.hackDashId
 				}
 				
-				console.log('$scope.hackDashId', $scope.hackDashId)
+				console.log('$scope.hackDashId', scope.hackDashId)
 
-				$scope.test = 'test'
+				scope.test = 'test'
 
-				$http.get('https://hackdash.org/api/v2/projects/'+$scope.hackDashId)
+				$http.get('https://hackdash.org/api/v2/projects/'+scope.hackDashId)
 				.then(function(result){
-					$scope.hackDashData = result.data
-					console.log($scope.hackDashData)
+					scope.hackDashData = result.data
+					console.log(scope.hackDashData)
 				})
 
 			}
