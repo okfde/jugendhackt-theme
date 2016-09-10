eaad9ce3b82a6ebc6365bfabd7db19f75872bce5
rhotep <andreas.pittrich@posteo.de>
2016-09-11 00:30:08 +0200

    sdf

--- library/js/app.js
+++ library/js/app.js
@@ -7,13 +7,19 @@ angular.module('jugendHackt', [])
 		return {
 			restrict: 	"AE",
 			scope:		{
-							teaserData : "<",
-							hackDashId : "<",
+							teaserData : "@",
+							hackDashId : "@",
 						},
 
 			link: function($scope){
 				console.log('TeaserData:', $scope.teaserData)
 				console.log('hackDashId:', $scope.hackDashId)
+
+				var id_in_embed_snippet  = $scope.hackDashId.match(/hackdash.org\/embed\/projects\/(.*)\?/g)
+
+				if(embed_snippet != null){
+					console.dir(id_in_embed_snippet)
+				}
 			}
 		}
 	}
