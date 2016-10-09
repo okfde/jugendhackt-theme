3b77b5066d0464ffdad2ed415a4de4592d67e569
rhotep <andreas.pittrich@posteo.de>
2016-10-09 18:43:01 +0200

    sddsf

--- page-projekte.php
+++ page-projekte.php
@@ -31,8 +31,8 @@ $args = array(
 	'child_of'                 => $year_parent_id,
 	'orderby'                  => 'name',
 	'order'                    => 'ASC',
-	'hide_empty'               => 1,
-	'hierarchical'             => 1,
+	'hide_empty'               => 0,
+	'hierarchical'             => 0,
 	'taxonomy'                 => 'page_category',
 ); 
 
