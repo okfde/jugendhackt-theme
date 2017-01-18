a7b4d5e80979516c0d5b9e58905c290a50766bb0
rhotep <andreas.pittrich@posteo.de>
2017-01-18 19:07:47 +0100

    working on #25

--- page-projekte.php
+++ page-projekte.php
@@ -93,13 +93,9 @@ $field = get_field_object('hackdash_projects');
 				<h2 class="filter-year active" data-filter-year ="*" >[alle]</h2>
 				<?php 
 					arsort($years);
-
-					var_dump($years);
 					if(!empty($years)) {
 							foreach ($years as $key => $year) { 
-				?>
-								<h2 class="filter-year" data-filter-year = ".filter-me-as-<?php echo $year ?>" > <?php echo $year  ?> </h2>
-				<?php 	
+				?><h2 class="filter-year" data-filter-year = ".filter-me-as-<?php echo $year ?>" > <?php echo $year  ?> </h2><?php 	
 						}
 					} 
 				?> 
