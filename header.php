0998c50f95243a5bc2ec4d62c38c78cebbd348d2
rhotep <andreas.pittrich@posteo.de>
2017-01-18 15:23:47 +0100

    working on #37

--- header.php
+++ header.php
@@ -47,6 +47,25 @@
 
 <body <?php body_class(); ?>  >
 
+	<!-- Piwik -->
+	<script type="text/javascript">
+		var _paq = _paq || [];
+		_paq.push(["setDomains", ["*.jugendhackt.org","*.jugendhackt.org"]]);
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u="//traffic.okfn.de/";
+			_paq.push(['setTrackerUrl', u+'piwik.php']);
+			_paq.push(['setSiteId', '28']);
+			var d=document, g=d.createElement('script'),
+				s=d.getElementsByTagName('script')[0];
+				g.type='text/javascript'; g.async=true; g.defer=true;
+				g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+	  	})();
+	</script>
+	<noscript><p><img src="//traffic.okfn.de/piwik.php?idsite=28" style="border:0;" alt="" /></p></noscript>
+	<!-- End Piwik Code -->
+
 	<div id="container">
 
 		<header class="header" role="banner">
